a2a67b1e4a292624bad42b372ac189bcb3f69604
XTEP63 <estebanjbn@gmail.com>
2025-05-12 18:27:41 -0600

    pipeline

--- imedia_project/data_procesing/Reddit_pre_procesing.py
+++ imedia_project/data_procesing/Reddit_pre_procesing.py
@@ -1,101 +1,95 @@
 import json
 import pandas as pd
 
-# Cargar los datos del archivo JSON
-with open("../../data/raw/raw_reddit_data.json", "r") as file:
-    reddit_data = json.load(file)
-    
+class RedditPreProcessing:
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self.reddit_data = self.load_data()
 
-# Crear DataFrames vacíos para cada tipo de información
-comments_df = pd.DataFrame(columns=['author', 'body', 'user_subscriptions', 'subreddit'])
-ultimos_posts_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
-top_posts_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
-posts_relacionados_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
-subreddit_info_df = pd.DataFrame(columns=['name', 'subscribers', 'description', 'subreddit'])
+    def load_data(self):
+        """Carga los datos del archivo JSON"""
+        with open(f"{self.base_path}/data/raw/raw_reddit_data.json", "r") as file:
+            return json.load(file)
 
-# Listas temporales para almacenar los datos
-comments_data = []
-ultimos_posts_data = []
-top_posts_data = []
-posts_relacionados_data = []
-subreddit_info_data = []  # Lista para almacenar la información de subreddit
+    def process(self):
+        """Procesa los datos de Reddit y los convierte a DataFrame"""
+        # Crear DataFrames vacíos para cada tipo de información
+        comments_df = pd.DataFrame(columns=['author', 'body', 'user_subscriptions', 'subreddit'])
+        ultimos_posts_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
+        top_posts_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
+        posts_relacionados_df = pd.DataFrame(columns=['title', 'score', 'url', 'subreddit'])
+        subreddit_info_df = pd.DataFrame(columns=['name', 'subscribers', 'description', 'subreddit'])
 
-# Recorrer cada subreddit y sus datos
-for subreddit, data in reddit_data.items():
-    # Procesar comentarios
-    for comment in data.get('comentarios_mas_recientes', []):
-        comment_data = {
-            'author': comment.get('author'),
-            'body': comment.get('body'),
-            'user_subscriptions': comment.get('user_subscriptions'),
-            'subreddit': subreddit
-        }
-        comments_data.append(comment_data)
+        # Listas temporales para almacenar los datos
+        comments_data = []
+        ultimos_posts_data = []
+        top_posts_data = []
+        posts_relacionados_data = []
+        subreddit_info_data = []
 
-    # Procesar últimos posts
-    for post in data.get('ultimos_posts', []):
-        post_data = {
-            'title': post.get('title'),
-            'score': post.get('score'),
-            'url': post.get('url'),
-            'subreddit': subreddit
-        }
-        ultimos_posts_data.append(post_data)
+        # Recorrer cada subreddit y sus datos
+        for subreddit, data in self.reddit_data.items():
+            # Procesar comentarios
+            for comment in data.get('comentarios_mas_recientes', []):
+                comment_data = {
+                    'author': comment.get('author'),
+                    'body': comment.get('body'),
+                    'user_subscriptions': comment.get('user_subscriptions'),
+                    'subreddit': subreddit
+                }
+                comments_data.append(comment_data)
 
-    # Procesar top posts
-    for post in data.get('top_posts', []):
-        post_data = {
-            'title': post.get('title'),
-            'score': post.get('score'),
-            'url': post.get('url'),
-            'subreddit': subreddit
-        }
-        top_posts_data.append(post_data)
+            # Procesar últimos posts
+            for post in data.get('ultimos_posts', []):
+                post_data = {
+                    'title': post.get('title'),
+                    'score': post.get('score'),
+                    'url': post.get('url'),
+                    'subreddit': subreddit
+                }
+                ultimos_posts_data.append(post_data)
 
-    # Procesar posts relacionados
-    for post in data.get('posts_relacionados', []):
-        post_data = {
-            'title': post.get('title'),
-            'score': post.get('score'),
-            'url': post.get('url'),
-            'subreddit': subreddit
-        }
-        posts_relacionados_data.append(post_data)
+            # Procesar top posts
+            for post in data.get('top_posts', []):
+                post_data = {
+                    'title': post.get('title'),
+                    'score': post.get('score'),
+                    'url': post.get('url'),
+                    'subreddit': subreddit
+                }
+                top_posts_data.append(post_data)
 
-    # Procesar información del subreddit
-    subreddit_info = data.get('subreddit_info', {})
-    subreddit_info_data.append({
-        'name': subreddit_info.get('name'),
-        'subscribers': subreddit_info.get('subscribers'),
-        'description': subreddit_info.get('description'),
-        'subreddit': subreddit
-    })
+            # Procesar posts relacionados
+            for post in data.get('posts_relacionados', []):
+                post_data = {
+                    'title': post.get('title'),
+                    'score': post.get('score'),
+                    'url': post.get('url'),
+                    'subreddit': subreddit
+                }
+                posts_relacionados_data.append(post_data)
 
-# Crear los DataFrames usando pd.concat()
-comments_df = pd.concat([comments_df, pd.DataFrame(comments_data)], ignore_index=True)
-ultimos_posts_df = pd.concat([ultimos_posts_df, pd.DataFrame(ultimos_posts_data)], ignore_index=True)
-top_posts_df = pd.concat([top_posts_df, pd.DataFrame(top_posts_data)], ignore_index=True)
-posts_relacionados_df = pd.concat([posts_relacionados_df, pd.DataFrame(posts_relacionados_data)], ignore_index=True)
-subreddit_info_df = pd.concat([subreddit_info_df, pd.DataFrame(subreddit_info_data)], ignore_index=True)
+            # Procesar información del subreddit
+            subreddit_info = data.get('subreddit_info', {})
+            subreddit_info_data.append({
+                'name': subreddit_info.get('name'),
+                'subscribers': subreddit_info.get('subscribers'),
+                'description': subreddit_info.get('description'),
+                'subreddit': subreddit
+            })
 
-# Ver los primeros registros de cada DataFrame
-print("Comentarios (primeros 5 registros):")
-print(comments_df.head())
+        # Crear los DataFrames usando pd.concat()
+        comments_df = pd.concat([comments_df, pd.DataFrame(comments_data)], ignore_index=True)
+        ultimos_posts_df = pd.concat([ultimos_posts_df, pd.DataFrame(ultimos_posts_data)], ignore_index=True)
+        top_posts_df = pd.concat([top_posts_df, pd.DataFrame(top_posts_data)], ignore_index=True)
+        posts_relacionados_df = pd.concat([posts_relacionados_df, pd.DataFrame(posts_relacionados_data)], ignore_index=True)
+        subreddit_info_df = pd.concat([subreddit_info_df, pd.DataFrame(subreddit_info_data)], ignore_index=True)
 
-print("Últimos posts (primeros 5 registros):")
-print(ultimos_posts_df.head())
+        # Guardar los DataFrames como archivos CSV
+        comments_df.to_csv(f'{self.base_path}/data/interim/comments_data.csv', index=False)
+        ultimos_posts_df.to_csv(f'{self.base_path}/data/interim/ultimos_posts_data.csv', index=False)
+        top_posts_df.to_csv(f'{self.base_path}/data/interim/top_posts_data.csv', index=False)
+        posts_relacionados_df.to_csv(f'{self.base_path}/data/interim/posts_relacionados_data.csv', index=False)
+        subreddit_info_df.to_csv(f'{self.base_path}/data/interim/subreddit_info_data.csv', index=False)
 
-print("Top posts (primeros 5 registros):")
-print(top_posts_df.head())
-
-print("Posts relacionados (primeros 5 registros):")
-print(posts_relacionados_df.head())
-
-print("Información del subreddit (primeros 5 registros):")
-print(subreddit_info_df.head())
-
-comments_df.to_csv('../../data/interim/comments_data.csv', index=False)
-ultimos_posts_df.to_csv('../../data/interim/ultimos_posts_data.csv', index=False)
-top_posts_df.to_csv('../../data/interim/top_posts_data.csv', index=False)
-posts_relacionados_df.to_csv('../../data/interim/posts_relacionados_data.csv', index=False)
-subreddit_info_df.to_csv('../../data/interim/subreddit_info_data.csv', index=False)
+        print("Datos procesados y guardados con éxito.")
---
+++ imedia_project/pipeline.py
@@ -0,0 +1,48 @@
+# from pipeline import Pipeline
+from data_extraction.extrac_from_reddit.create_reddit_raw_data import CreateRawRedditData
+from data_extraction.extrac_from_reddit.Reddit_extract import Reddit_extraction
+from data_procesing.Reddit_pre_procesing import RedditPreProcessing
+from data_cleaning.data_cleaning import DataProcessor
+import os
+
+class RedditPipeline:
+    def __init__(self, base_path, post_limit=100, comment_limit=100, subreddits=None):
+        self.base_path = base_path
+        self.post_limit = post_limit
+        self.comment_limit = comment_limit
+        self.subreddits = subreddits if subreddits else []
+
+    def extract_data(self):
+        """Fase de extracción de datos"""
+        reddit_extraction = Reddit_extraction()
+        trending_subreddits = reddit_extraction.get_trending_subreddits(1000*4)
+        trending_subreddits = set(trending_subreddits)
+        print(f'-------------------------Tenemos {len(trending_subreddits)} subreddits que son {trending_subreddits}-------------------------')
+
+        reddit_data = CreateRawRedditData(trending_subreddits, post_limit=self.post_limit, comment_limit=self.comment_limit)
+        print("Recopilando datos de Reddit...")
+        reddit_data.fetch_data()
+
+        reddit_data.save_to_json(os.path.join(self.base_path, "data/raw/raw_reddit_data.json"))
+
+    def preprocess_data(self):
+        """Fase de preprocesamiento de datos"""
+        processor = RedditPreProcessing(self.base_path)
+        processor.process_all()
+
+    def clean_data(self):
+        """Fase de limpieza de datos"""
+        processor = DataProcessor(self.base_path)
+        processor.process_all()
+
+    def run(self):
+        """Ejecuta el pipeline completo"""
+        self.extract_data()
+        self.preprocess_data()
+        self.clean_data()
+
+# Ejecutar el pipeline
+if __name__ == "__main__":
+    base_path = 'ruta/a/tu/proyecto'  # Cambia esto por la ruta real
+    pipeline = RedditPipeline(base_path)
+    pipeline.run()
